1cc8780f965cef37822509f42b747e6b3447dcdc
sarant1 <samuel.arant10@gmail.com>
2023-08-28 17:35:40 -0400

    feat: add animation for landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,14 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { SiGithub, SiLinkedin, SiLeetcode } from "react-icons/si";
+import { motion } from "framer-motion";
 
 const Landing: React.FC = () => {
   return (
     <div className="h-[92vh] flex justify-center flex-col items-center">
-      <p className="text-white text-2xl mb-4">HELLO, I AM</p>
-      <h1 className="text-blue font-bold text-8xl mb-8">SAMUEL ARANT</h1>
-      <p className="text-white text-2xl mb-8">WELCOME TO MY PORTFOLIO</p>
-      <div className="flex gap-4 mb-32">
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1.5, delay: 0.5 }}
+      >
+        <p className="text-white text-2xl mb-4">HELLO, I AM</p>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1.5, delay: 0.8 }}
+      >
+        <h1 className="text-blue font-bold text-8xl mb-8">SAMUEL ARANT</h1>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1.5, delay: 1.1 }}
+      >
+        <p className="text-white text-2xl mb-8">WELCOME TO MY PORTFOLIO</p>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1.5, delay: 1.1 }}
+        className="flex gap-4 mb-32"
+      >
         <Link to="https://github.com/sarant1" target="_blank">
           <button className="transform transition duration-300 hover:scale-110">
             <SiGithub size={32} className="text-white" />
@@ -27,7 +51,7 @@ const Landing: React.FC = () => {
             <SiLeetcode size={32} className="text-orange" />
           </button>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 };
