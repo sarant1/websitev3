b5d74ef8e59300de1b422d01d856b62c8364bb0f
sarant1 <samuel.arant10@gmail.com>
2024-01-09 16:46:41 -0500

    feat: create hamburger menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,92 @@
+"use client";
 import Link from "next/link";
+import React from "react";
+import { useState } from "react";
+import { slide as Menu } from "react-burger-menu";
 
-export default function HomePage() {
+export default function HomePage(): React.ReactNode {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const closeMenu = () => setIsOpen(false);
+  const handleStateChange = (state: any) => setIsOpen(state.isOpen);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+    <main>
+      <Menu
+        right
+        isOpen={isOpen}
+        width={"280px"}
+        styles={styles}
+        onStateChange={handleStateChange}
+      >
+        <ul className="space-y-4">
+          <li>
+            <Link href="/about" className="text-4xl font-bold hover:text-white">
+              about
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/projects"
+              className="text-4xl font-bold hover:text-white"
+            >
+              projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/contact"
+              className="text-4xl font-bold hover:text-white"
+            >
+              contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/contact"
+              className="text-4xl font-bold hover:text-white"
+            >
+              resume
+            </Link>
+          </li>
+        </ul>
+      </Menu>
+      <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <h1 className="tracking-tgiht font-extrabold text-[hsl(280,100%,70%)] sm:text-[5rem]">
+          Hello World, I'm
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Samuel Arant
+        </h1>
       </div>
     </main>
   );
 }
+
+const styles = {
+  bmBurgerButton: {
+    position: "fixed",
+    width: "36px",
+    height: "30px",
+    right: "36px",
+    top: "36px",
+  },
+  bmBurgerBars: {
+    background: "#373a47",
+  },
+  bmMenuWrap: {
+    position: "fixed",
+    height: "100%",
+  },
+  bmMenu: {
+    background: "#373a47",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
+  },
+  bmItemList: {
+    color: "#b8b7ad",
+    padding: "0.8em",
+  },
+  bmItem: {
+    display: "inline-block",
+  },
+};
