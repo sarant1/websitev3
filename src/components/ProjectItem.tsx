d7ee6583d2e54c9ad120cc716fc25c889c907426
sarant1 <samuel.arant10@gmail.com>
2024-01-12 19:34:34 -0500

    feat: add project item templates

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Divider } from "@chakra-ui/react";
 
 const ProjectItem: React.FC = () => {
   return (
-    <div className="container mx-3 h-[300px] rounded-lg border-2 border-white md:w-[300px]">
-      <h1>Project Item</h1>
+    <div className="container relative mx-2 h-[300px] rounded-lg border-2 border-[rgba(255,255,255,0.2)] bg-[rgba(159,90,253,0.2)] hover:border-[rgba(255,255,255,0.7)] md:w-[400px]">
+      <div className="rounded-t-md bg-[rgba(255,255,255,0.2)] p-3">
+        <h1 className="text-center">Project Item</h1>
+      </div>
+      <div className="">
+        <h3 className="px-3 pt-2">Tech Stack: </h3>
+        <p className="p-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          voluptate, voluptatum, voluptatibus, voluptas quae quos nesciunt
+          temporibus fugit quibusdam voluptatem doloremque? Quisquam voluptate,
+        </p>
+      </div>
     </div>
   );
 };
