a9d362ad79c1c41a1bcc99f7b391a2f16ca1e801
sarant1 <samuel.arant10@gmail.com>
2024-01-16 21:08:31 -0500

    feat: hamburger disappear when scrolling down

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,17 +1,34 @@
 "use client";
 import React from "react";
+import { useState, useEffect } from "react";
 import HamburgerMenu from "~/components/HamburgerMenu";
 import LinkIcons from "~/components/LinkIcons";
 import Header from "~/components/Header";
 
 const Landing = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY < 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  });
+
   return (
     <div>
-      <div className="block md:hidden">
-        <HamburgerMenu />
-      </div>
-      <div className="hidden md:block">
-        <Header />
+      <div className={isVisible ? "block" : "hidden"}>
+        <div className="block md:hidden">
+          <HamburgerMenu />
+        </div>
+        <div className="hidden md:block">
+          <Header />
+        </div>
       </div>
       <div className="flex min-h-screen flex-col items-center justify-center">
         <h1 className="font-extrabold text-[hsl(280,100%,70%)] sm:text-[5rem]">
