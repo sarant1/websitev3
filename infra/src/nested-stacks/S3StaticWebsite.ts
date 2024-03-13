3b340e1fd670f17eb64a9daa76dc44160b519290
sarant1 <samuel.arant10@gmail.com>
2024-03-13 10:40:55 -0400

    feat: things kinda work now

--- infra/src/nested-stacks/S3StaticWebsite.ts
+++ infra/src/nested-stacks/S3StaticWebsite.ts
@@ -5,21 +5,29 @@ Resources:
 
 */
 
-import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-import { Construct } from 'constructs';
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as iam from "aws-cdk-lib/aws-iam";
+// import * as route53 from "aws-cdk-lib/aws-route53";
+import { Construct } from "constructs";
+import { getAppRootDir } from "../utils/get-root-dir";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 interface S3StaticWebsiteProps extends cdk.NestedStackProps {
   bucketName: string;
+  certificateArn: string;
 }
 
 export default class S3StaticWebsiteStack extends cdk.NestedStack {
   public readonly bucket: s3.Bucket;
   public readonly cdn: cloudfront.CloudFrontWebDistribution;
+  public readonly certificateArn: string;
 
   constructor(scope: Construct, id: string, props: S3StaticWebsiteProps) {
     super(scope, id, props);
+    this.certificateArn = props.certificateArn;
 
     const s3Bucket = new s3.Bucket(scope, "MainWebsiteBucket", {
       bucketName: props.bucketName,
@@ -28,26 +36,52 @@ export default class S3StaticWebsiteStack extends cdk.NestedStack {
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
     });
 
+    new s3Deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3Deploy.Source.asset(`${getAppRootDir()}/../frontend/out`)],
+      destinationBucket: s3Bucket,
+    });
 
-      const cloudfrontDistribution = new cloudfront.CloudFrontWebDistribution(
+    // we must declare bucket before the cloudfront distribution
+
+    this.bucket = s3Bucket;
+
+    const s3BucketPolicy = new PolicyStatement({
+      actions: ["s3:GetObject"],
+      resources: [`${this.bucket.bucketArn}/*`],
+      principals: [new iam.AnyPrincipal()],
+      effect: iam.Effect.ALLOW,
+    });
+
+    this.bucket.addToResourcePolicy(s3BucketPolicy);
+
+    const cloudfrontDistribution = new cloudfront.CloudFrontWebDistribution(
       this,
       "portfolio-cloudfront-distribution",
       {
         originConfigs: [
           {
-            customOriginSource: {
-              domainName: s3Bucket.bucketWebsiteDomainName,
-              originProtocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+            s3OriginSource: {
+              s3BucketSource: this.bucket,
             },
             behaviors: [{ isDefaultBehavior: true }],
           },
         ],
+        viewerCertificate: {
+          aliases: [`${props.bucketName}`, `www.${props.bucketName}`],
+          props: {
+            acmCertificateArn: this.certificateArn, // optional
+            sslSupportMethod: "sni-only",
+            minimumProtocolVersion: "TLSv1.2_2021",
+            // All `props` options here: https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-cloudfront.CfnDistribution.ViewerCertificateProperty.html
+          },
+        },
       }
     );
 
-    this.bucket = s3Bucket;
     this.cdn = cloudfrontDistribution;
+
+    // const myZone = route53.HostedZone.fromLookup(this, "MyZone", {
+    //   domainName: props.bucketName,
+    // });
   }
 }
-
-
--- infra/src/root.ts
+++ infra/src/root.ts
@@ -5,19 +5,22 @@ import S3StaticWebsiteStack from "./nested-stacks/S3StaticWebsite";
 export class RootStack extends cdk.Stack {
   public projectName: string;
   public bucketName: string;
+  public certificateArn: string;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     this.projectName = "PortfolioWebsite";
     this.bucketName = "samuelarant.com";
+    this.certificateArn =
+      "arn:aws:acm:us-east-1:975050373052:certificate/c4a82c54-2e49-4ad8-a94e-bf5e514906d4";
 
-    this.createStaticWebsite(`${this.projectName}-Static-Website`)
+    this.createStaticWebsite(`${this.projectName}-Static-Website`);
   }
-
 
   createStaticWebsite(name: string) {
     return new S3StaticWebsiteStack(this, name, {
-      bucketName: this.bucketName
-    })
+      bucketName: this.bucketName,
+      certificateArn: this.certificateArn,
+    });
   }
 }
